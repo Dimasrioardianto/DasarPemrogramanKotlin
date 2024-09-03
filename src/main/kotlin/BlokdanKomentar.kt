d5c289514982f3b5a956fceadde629dd45f0aaa5
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-03 13:20:58 +0700

    add BlokdanKomentar.kt

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    println("Hello, Ini Program pertama saya di Kotlin")
+
+    //contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * kotlin doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
