e16dd45c155dcb305cf776a4e0859a35288c2413
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-03 12:12:31 +0700

    Project starter

---
+++ src/main/kotlin/Literal.kt
@@ -0,0 +1,12 @@
+fun main (args: Array<String>){
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+    println("intLiteral = " + intLiteral)
+    println("doubleLiteral = "+ doubleLiteral)
+    println("stringLiteral= "+ stringLiteral)
+    println("charLiteral ="+ charLiteral)
+    println("boolLiteral = "+ boolLiteral )
+}
