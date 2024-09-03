4ed2e5502c1ff9a9a6a99ef4330e8bf3ca747992
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-03 13:42:41 +0700

    add Valvariabel.kt

---
+++ src/main/kotlin/valVariabel.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varvariabel
+    println("Pemberian nilai pertama pada varVariabel= "+varVariabel)
+    varVariabel = 78 //assigning second value to varvariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+    var phi = 3.14  //saat menggunakan val phi maka phi akan merah(error) maka saya ganti ke var
+    phi = 2.5
+}
