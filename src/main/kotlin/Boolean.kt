45212f7f4bfecc0af661c2cecde51521bdd6bfb7
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-07 22:41:21 +0700

    add Boolean.kt

--- src/main/kotlin/Boolean.kt
+++ src/main/kotlin/Boolean.kt
@@ -2,9 +2,8 @@ fun main(args: Array<String>) {
     var varBool: Boolean = true
     if (varBool)
         println("varBool bernilai " + varBool)
-}
-/**
+
     var varInt :Int = 1
-    if(varInt) : = perbaikan menurut saya kondisi if disini harus menghasilkan true/false contohnya if(varint >0)
-        println("varInt bernilai "+varBool ) =dan juga ini harusnya varInt
-}**/
+    if(varInt != 0) //kondisi if harusnya memiliki tipe bolean karena varInt adalah Int jadi saya menambahkan kondisi seperti varInt !=0
+        println("varInt bernilai "+varInt )
+}
