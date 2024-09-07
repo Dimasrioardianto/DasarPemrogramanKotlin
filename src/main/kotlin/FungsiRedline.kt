a049a7aee2522109aa35f3e375bd34b61c3b689b
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-07 13:30:09 +0700

    add FungsiRedline.kt

---
+++ src/main/kotlin/FungsiRedline.kt
@@ -0,0 +1,11 @@
+//perintah masukkan dikotlin yaitu fungsi redline, perintah ini akan mengembalikan nilai string ke dlm variabel penampung
+fun main(args :Array<String>){
+    //program burung beo
+    println("Hi i'm Lora, the vitual parrot, and i love to repeat!");
+    println("Type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + "," + input + "!"
+    println(output)
+}
