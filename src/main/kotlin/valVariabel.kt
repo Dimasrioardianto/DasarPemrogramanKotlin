bd17fca7af3e344d14d62fcfd9cbe25af31ed1f1
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-08 21:16:10 +0700

    add valVariabel.kt

--- src/main/kotlin/valVariabel.kt
+++ src/main/kotlin/valVariabel.kt
@@ -4,6 +4,8 @@ fun main(args: Array<String>){
     println("Pemberian nilai pertama pada varVariabel= "+varVariabel)
     varVariabel = 78 //assigning second value to varvariabel
     println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
-    var phi = 3.14  //saat menggunakan val phi maka phi akan merah(error) maka saya ganti ke var
+    var phi = 3.14  //phi dari val diubah ke var,sehingga program bisa dijalankan
+    println("Nilai awal phi ="+phi)//disini jg saya tambah pritnln agar nilai phi dapat dicetak
     phi = 2.5
+    println("Nilai baru phi =" + phi)
 }
