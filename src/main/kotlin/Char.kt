68a85c12ae8772f7e8f7e2a3f08895b2a3ffa0a6
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-08 21:22:33 +0700

    add Char.kt

--- src/main/kotlin/Char.kt
+++ src/main/kotlin/Char.kt
@@ -1,6 +1,7 @@
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     var charA: Char = 'A'
-    // println(charA == 65) =
+    //println(charA == 65) = 65 diganti A karena 65 Int & A char
+
     println(charA.isUpperCase())
     println(charA.isLowerCase())
     println(charA.isDigit())
