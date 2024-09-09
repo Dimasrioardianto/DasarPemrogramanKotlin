7206217fbe94c53bf39ba236a62370a3ee6f2422
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-09-09 23:05:52 +0700

    add Variabel.kt

--- src/main/kotlin/Variabel.kt
+++ src/main/kotlin/Variabel.kt
@@ -1,3 +1,4 @@
+//variabel yaitu sebagai wadah didlm  memori komputer utk menyimpan data
 fun main(args: Array<String>){
     var dynamicVar = 2
     println("Contoh dynamic variabel = "+dynamicVar)
